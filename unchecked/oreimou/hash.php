f6d92925dc795c42e6a99368754cd037dab4f458
qiusj <qiusj@mahou-shoujo.moe>
2016-03-14 16:49:32 +0800

    ass files collect

---
+++ unchecked/oreimou/hash.php
@@ -0,0 +1,22 @@
+<?php
+
+$flist = scandir(".");
+$htbl = [];
+
+if (!file_exists("duplicate")) {
+	mkdir("duplicate");
+}
+
+foreach ($flist as $file) {
+	if (in_array($file, [".", "..", basename(__FILE__)]) or is_dir($file)) {
+		continue;
+	}
+
+	$hash = sha1_file($file);
+	if (in_array($hash, $htbl)) {
+		rename($file, "duplicate/{$file}");
+		echo "Duplicate {$file}". PHP_EOL;
+	} else {
+		$htbl[] = $hash;
+	}
+}
